9a0bbc3b633855c357607e34241ba2792f480d5c
Viru154 <nerygalvezh@gmail.com>
2024-08-14 20:28:51 -0600

    4to Commit

--- app/routers/router.js
+++ app/routers/router.js
@@ -2,15 +2,16 @@
 let express = require('express');
 let router = express.Router();
  
-const libros = require('../controllers/controller.js');
+const Libros = require('../controllers/controller.js');
 
-router.post('/api/song/create', song.create);
+router.post('/api/Libros/create', Libros.create)
+/*
 router.get('/api/song/all', song.retrieveAllSong);
 router.get('/api/song/onebyid/:id', song.getSongById);
 router.get('/api/song/filteringbyname', song.filteringByName);
 //router.get('/api/song/pagination', song.pagination);
 //router.get('/api/song/pagefiltersort', song.pagingfilteringsorting);
 router.put('/api/song/update/:id', song.updateById);
-router.delete('/api/song/delete/:id', song.deleteById);
-
+router.delete('/api/song/delete/:id', song.deleteById); 
+*/
 module.exports = router;
